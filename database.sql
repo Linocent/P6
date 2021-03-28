5c1fc236e4fbbe228adc8ddfc9c96b35ee971aa4
Linocent <timotheelinocent@gmail.com>
2021-03-28 10:24:00 +0200

    Modification et création db p6.

--- database.sql
+++ database.sql
@@ -1,89 +1,51 @@
-DROP DATABASE IF EXISTS p6;
 
-CREATE DATABASE p6
-WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'French_France.1252'
-    LC_CTYPE = 'French_France.1252'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
-
-
-CREATE TABLE IF NOT EXISTS utilisateur(
-    nom VARCHAR(255) NOT NULL,
-    prenom VARCHAR(255) NOT NULL,
-    id SERIAL PRIMARY KEY
+CREATE TABLE utilisateur(
+    nom varchar(255) NOT NULL,
+    prenom varchar(255) NOT NULL,
+	identifiant serial PRIMARY KEY
 );
 
-CREATE TABLE IF NOT EXISTS caissier(
-    nom_utilisateur VARCHAR(255) NOT NULL,
-    prenom_utilisateur VARCHAR(255) NOT NULL,
-    numero_utilisateur SERIAL PRIMARY KEY,
-    CONSTRAINT fk_nom_client
-        FOREIGN KEY (nom_utilisateur)
-        REFERENCES utilisateur(prenom),
-    CONSTRAINT fk_prenom_client
-        FOREIGN KEY (prenom_utilisateur)
-        REFERENCES utilisateur(prenom)
+CREATE TABLE caissier(
+    identifiant SERIAL PRIMARY KEY,
+    CONSTRAINT fk_numero_utilisateur
+        FOREIGN KEY (identifiant)
+        REFERENCES utilisateur(identifiant)
 );
 
-CREATE TABLE IF NOT EXISTS livreur(
-    nom_utilisateur VARCHAR(255) NOT NULL,
-    prenom_utilisateur VARCHAR(255) NOT NULL,
-    numero_utilisateur SERIAL PRIMARY KEY,
-    CONSTRAINT fk_nom_client
-        FOREIGN KEY (nom_utilisateur)
-        REFERENCES utilisateur(prenom),
-    CONSTRAINT fk_prenom_client
-        FOREIGN KEY (prenom_utilisateur)
-        REFERENCES utilisateur(prenom)
+CREATE TABLE livreur(
+    identifiant SERIAL PRIMARY KEY,
+    CONSTRAINT fk_numero_utilisateur
+        FOREIGN KEY (identifiant)
+        REFERENCES utilisateur(identifiant)
 );
 
-CREATE TABLE IF NOT EXISTS pizzaiolo(
-    nom_utilisateur VARCHAR(255) NOT NULL,
-    prenom_utilisateur VARCHAR(255) NOT NULL,
-    numero_utilisateur SERIAL PRIMARY KEY,
-    CONSTRAINT fk_nom_client
-        FOREIGN KEY (nom_utilisateur)
-        REFERENCES utilisateur(prenom),
-    CONSTRAINT fk_prenom_client
-        FOREIGN KEY (prenom_utilisateur)
-        REFERENCES utilisateur(prenom)
+CREATE TABLE pizzaiolo(
+    identifiant SERIAL PRIMARY KEY,
+    CONSTRAINT fk_numero_utilisateur
+        FOREIGN KEY (identifiant)
+        REFERENCES utilisateur(identifiant)
 );
 
-CREATE TABLE IF NOT EXISTS client(
-    nom_client VARCHAR(255) NOT NULL,
-    prenom_client VARCHAR(255) NOT NULL,
-    numero_utilisateur SERIAL PRIMARY KEY,
-    CONSTRAINT fk_nom_client
-        FOREIGN KEY (nom_client)
-        REFERENCES utilisateur(prenom),
-    CONSTRAINT fk_prenom_client
-        FOREIGN KEY (prenom_client)
-        REFERENCES utilisateur(prenom)
+CREATE TABLE client(
+    identifiant SERIAL PRIMARY KEY,
+    CONSTRAINT fk_numero_client
+        FOREIGN KEY (identifiant)
+        REFERENCES utilisateur(identifiant)
 );
 
-CREATE TABLE IF NOT EXISTS information_utilisateur(
-    nom VARCHAR(255) NOT NULL,
-    prenom VARCHAR(255) NOT NULL,
-    id SERIAL PRIMARY KEY
-);
-
-CREATE TABLE IF NOT EXISTS information(
+CREATE TABLE information(
     rue VARCHAR(255) NOT NULL,
     ville VARCHAR (255) NOT NULL,
     code_postal INT NOT NULL,
     information_complementaire VARCHAR (255) NOT NULL,
     adresse_email VARCHAR(255) NOT NULL,
-    numero_utilisateur INT NOT NULL,
-    id SERIAL PRIMARY KEY,
+    identifiant SERIAL PRIMARY KEY,
     CONSTRAINT fk_numero_utilisateur
-        FOREIGN KEY (numero_utilisateur)
-        REFERENCES client(numero_utilisateur)
+        FOREIGN KEY (identifiant)
+        REFERENCES client(identifiant)
 );
 
-CREATE TABLE IF NOT EXISTS commande(
+CREATE TABLE commande(
     date_time timestamp NOT NULL,
     etat_paiement VARCHAR(6),
     statut_commande VARCHAR(20),
@@ -94,37 +56,37 @@ CREATE TABLE IF NOT EXISTS commande(
     pizzaiolo_numero_utilisateur INT,
     CONSTRAINT fk_livreur_numero_utilisateur
         FOREIGN KEY (livreur_numero_utilisateur)
-        REFERENCES livreur(numero_utilisateur),
+        REFERENCES livreur(identifiant),
     CONSTRAINT fk_client_numero_utilisateur
         FOREIGN KEY (client_numero_utilisateur)
-        REFERENCES client(numero_utilisateur),
+        REFERENCES client(identifiant),
     CONSTRAINT fk_caissier_numero_utilisateur
         FOREIGN KEY (caissier_numero_utilisateur)
-        REFERENCES caissier(numero_utilisateur),
+        REFERENCES caissier(identifiant),
     CONSTRAINT fk_pizzaiolo_numero_utilisateur
         FOREIGN KEY (pizzaiolo_numero_utilisateur)
-        REFERENCES pizzaiolo(numero_utilisateur)
+        REFERENCES pizzaiolo(identifiant)
 );
 
-CREATE TABLE IF NOT EXISTS restaurant(
+CREATE TABLE restaurant(
     nom_restaurant VARCHAR(255) NOT NULL,
     id SERIAL PRIMARY KEY
 );
 
-CREATE TABLE IF NOT EXISTS ingredient(
+CREATE TABLE ingredient(
     nom VARCHAR(255)NOT NULL,
     description VARCHAR(255) NOT NULL,
     id SERIAL PRIMARY KEY
 );
 
-CREATE TABLE IF NOT EXISTS pizza(
+CREATE TABLE pizza(
     nom VARCHAR(255) NOT NULL,
     statut VARCHAR(20) NOT NULL,
     recette VARCHAR(255) NOT NULL,
     id SERIAL PRIMARY KEY
 );
 
-CREATE TABLE IF NOT EXISTS pizza_commande(
+CREATE TABLE pizza_commande(
     quantite INT NOT NULL,
     date date,
     pizza_id INT,
@@ -137,7 +99,7 @@ CREATE TABLE IF NOT EXISTS pizza_commande(
         REFERENCES commande(numero_de_commande)
 );
 
-CREATE TABLE IF NOT EXISTS pizza_restaurant(
+CREATE TABLE pizza_restaurant(
     pizza_id INT,
     restaurant_id INT,
     CONSTRAINT fk_pizza_id
@@ -148,7 +110,7 @@ CREATE TABLE IF NOT EXISTS pizza_restaurant(
         REFERENCES restaurant(id)
 );
 
-CREATE TABLE IF NOT EXISTS ingredient_restaurant(
+CREATE TABLE ingredient_restaurant(
     quantite INT,
     date date,
     ingredient_id INT,
@@ -161,7 +123,7 @@ CREATE TABLE IF NOT EXISTS ingredient_restaurant(
         REFERENCES restaurant(id)
 );
 
-CREATE TABLE IF NOT EXISTS pizza_ingredient(
+CREATE TABLE pizza_ingredient(
     pizza_id INT,
     ingredient_id INT,
     CONSTRAINT fk_pizza_id
--- donnee_demo.sql
+++ donnee_demo.sql
@@ -1,34 +1,37 @@
-INSERT INTO Pizza (nom, recette)
+INSERT INTO pizza (nom, recette)
     VALUES('Pizza_1', 'Recette Pizza_1'),
            ('Pizza_2', 'Recette Pizza_2'),
            ('Pizza_3', 'Recette Pizza_3'),
            ('Pizza_4', 'Recette Pizza_4');
 
-INSERT INTO Livreur(nom, prenom)
+INSERT INTO livreur(nom, prenom)
     VALUES('Livreur', 'numero 1');
 
-INSERT INTO Caissier(nom, prenom)
+INSERT INTO caissier(nom, prenom)
     VALUES('Caissier', 'numero 1');
 
-INSERT INTO Pizzaiolo(nom, prenom)
+INSERT INTO pizzaiolo(nom, prenom)
     VALUES('Pizzaiolo', 'numero 1');
 
-INSERT INTO Client(nom, prenom)
+INSERT INTO client(nom, prenom)
     VALUES('Client1', 'numero 1'),
            ('Client2', 'numero 2'),
            ('Client3', 'numero 3');
 
-INSERT INTO Restaurant(nom_restaurant)
+INSERT INTO restaurant(nom_restaurant)
     VALUES('Restaurant 1'),
            ('Restaurant 2'),
            ('Restaurant 3');
 
-INSERT INTO Ingredient(nom, description)
+INSERT INTO ingredient(nom, description)
     VALUES('Ingrédient 1', 'Description 1'),
            ('Ingrédient 2', 'Description 2'),
            ('Ingrédient 3', 'Description 3');
 
-INSERT INTO Information(rue, ville, code_postal, information_complementaire, adresse_email, numero_utilisateur)
-    VALUES('rue 1', 'Ville 1', 'code postal 1', 'information 1', 'email 1', Client.Numero_Utilisateur(Client1)),
-           ('rue 1', 'Ville 2', 'code postal 2', 'information 2', 'email 2', Client.Numero_Utilisateur(Client2)),
-           ('rue 1', 'Ville 3', 'code postal 3', 'information 3', 'email 3', Client.Numero_Utilisateur(Client3));
+INSERT INTO information(rue, ville, code_postal, information_complementaire, adresse_email, numero_utilisateur)
+    VALUES('rue 1', 'Ville 1', 'code postal 1', 'information 1', 'email 1',
+           (SELECT numero_utilisateur FROM client WHERE nom=Client1)),
+           ('rue 1', 'Ville 2', 'code postal 2', 'information 2', 'email 2',
+           (SELECT numero_utilisateur FROM client WHERE nom=Client2)),
+           ('rue 1', 'Ville 3', 'code postal 3', 'information 3', 'email 3',
+           (SELECT numero_utilisateur FROM client WHERE nom=Client3));
