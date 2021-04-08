a9abcf4f1caa1df080450e5c2dc3deda102ef86e
Linocent <timotheelinocent@gmail.com>
2021-04-08 10:54:44 +0200

    Ajout insert pour table commande, client, etc.

--- donnee_demo.sql
+++ donnee_demo.sql
@@ -1,20 +1,8 @@
-INSERT INTO pizza (nom, recette)
-    VALUES('Pizza_1', 'Recette Pizza_1'),
-           ('Pizza_2', 'Recette Pizza_2'),
-           ('Pizza_3', 'Recette Pizza_3'),
-           ('Pizza_4', 'Recette Pizza_4');
-
-INSERT INTO livreur(nom, prenom)
-    VALUES('Livreur', 'numero 1');
-
-INSERT INTO caissier(nom, prenom)
-    VALUES('Caissier', 'numero 1');
-
-INSERT INTO pizzaiolo(nom, prenom)
-    VALUES('Pizzaiolo', 'numero 1');
-
-INSERT INTO client(nom, prenom)
-    VALUES('Client1', 'numero 1'),
+INSERT INTO utilisateur(nom, prenom)
+    VALUES('Livreur', 'numero 1'),
+           ('Caissier', 'numero 1'),
+           ('Pizzaiolo', 'numero 1'),
+           ('Client1', 'numero 1'),
            ('Client2', 'numero 2'),
            ('Client3', 'numero 3');
 
@@ -28,10 +16,37 @@ INSERT INTO ingredient(nom, description)
            ('Ingrédient 2', 'Description 2'),
            ('Ingrédient 3', 'Description 3');
 
-INSERT INTO information(rue, ville, code_postal, information_complementaire, adresse_email, numero_utilisateur)
-    VALUES('rue 1', 'Ville 1', 'code postal 1', 'information 1', 'email 1',
-           (SELECT numero_utilisateur FROM client WHERE nom=Client1)),
-           ('rue 1', 'Ville 2', 'code postal 2', 'information 2', 'email 2',
-           (SELECT numero_utilisateur FROM client WHERE nom=Client2)),
-           ('rue 1', 'Ville 3', 'code postal 3', 'information 3', 'email 3',
-           (SELECT numero_utilisateur FROM client WHERE nom=Client3));
+INSERT INTO information(rue, ville, code_postal, information_complementaire, adresse_email, identifiant)
+    VALUES('rue 1', 'Ville 1', '01', 'information 1', 'email 1', '1'),
+           ('rue 1', 'Ville 2', '02', 'information 2', 'email 2', '1'),
+           ('rue 1', 'Ville 3', '03', 'information 3', 'email 3', '1');
+
+INSERT INTO pizza (nom, recette, statut)
+    VALUES('Pizza_1', 'Recette Pizza_1', 'En stock'),
+           ('Pizza_2', 'Recette Pizza_2', 'En stock'),
+           ('Pizza_3', 'Recette Pizza_3', 'En stock'),
+           ('Pizza_4', 'Recette Pizza_4', 'En stock');
+
+INSERT INTO client (identifiant)
+    VALUES('1'),
+           ('2'),
+           ('3');
+
+INSERT INTO caissier (identifiant)
+    VALUES('2'),
+           ('8');
+
+INSERT INTO livreur (identifiant)
+    VALUES('1'),
+           ('7');
+
+INSERT INTO pizzaiolo (identifiant)
+    VALUES('3'),
+           ('9');
+
+INSERT INTO commande (date_time, etat_paiement, statut_commande, client_numero_utilisateur)
+    VALUES('2021-04-08 19:10:25', 'paye', 'en preparation', '1'),
+           ('2021-04-08 19:10:25', 'impaye', 'en preparation', '2');
+
+INSERT INTO commande (date_time, etat_paiement, statut_commande, caissier_numero_utilisateur)
+    VALUES('2021-04-08 19:10:25', 'paye', 'en preparation', '2');
